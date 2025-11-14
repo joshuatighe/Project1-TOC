a020cfae865a63d1c82e953838c9fa80df865699
Joshua Tighe <jtighe3@nd.edu>
2025-11-13 19:00:34 -0500

    result plotting initial

--- src/plots.py
+++ src/plots.py
@@ -1,31 +1,49 @@
-from src.helpers.constants import INPUT_FILE
-from src.graph_coloring import GraphColoring 
-import matplotlib as plt
+import csv
 
-def plot(results):
+import matplotlib.pyplot as plt
 
-    xGreen = []
-    yGreen = [] 
-    xRed = []
-    yRed = []
+def parse_results(csv_path):
+    with open(csv_path, "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
 
-    for result in results:
+        results = []
+        for row in csv_reader:
+            results.append(row)
 
-        if result[5] == "NO":
-            xRed.append(result[1])
-            yRed.append(float(result[6]))
-        else:
-            xGreen.append(result[1])
-            yGreen.append(float(result[6]))
+    return results
 
-    plt.scatter(xRed, yRed, color="red")
-    plt.scatter(xGreen, yGreen, color="green")
+def plot_results(all_results):
+    bf_X = [result[6] for result in all_results[0]]
+    bf_Y = [result[3] for result in all_results[0]]
 
-    plt.title("Size of Problem vs. Execution Time")
-    plt.xlabel("Number of Vertices")
-    plt.ylabel("Time")
-    plt.grid(True)
-    plt.show()
+    bt_X = [result[6] for result in all_results[1]]
+    bt_Y = [result[3] for result in all_results[1]]
 
-if __name__ == "__main__":
-    plot()
+    fig, ax = plt.subplots()
+    ax.scatter(bf_X, bf_Y, color="red")
+    ax.scatter(bt_X, bt_Y, color="green")
+    ax.set_title("Size of Problem vs. Execution Time")
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Number of Colors")
+    ax.grid(True)
+
+    plt.savefig('../results/graph_input_graph_coloring_results.png')
+
+bf_results_csv_path = '../results/brute_force_graph_input_graph_coloring_results.csv'
+bt_results_csv_path = '../results/btracking_graph_input_graph_coloring_results.csv'
+
+# results indices
+# 0 -> instance_id
+# 1 -> n_vertices
+# 2 -> n_edges
+# 3 -> k
+# 4 -> method
+# 5 -> colorable
+# 6 -> time_seconds
+# 7 -> coloring
+bf_results = parse_results(bf_results_csv_path)
+bt_results = parse_results(bt_results_csv_path)
+
+all_results = (bf_results, bt_results)
+
+plot_results(all_results)
